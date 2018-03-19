ac636c26a49207dc37263b4362c7b17f195c3945
Xuan Wang <xuanwang@Xuans-MBP.fios-router.home>
2018-03-18 21:41:16 -0400

    update scaffolding

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+  name='jettie',
+  version='0.1',
+  description="a lightweight, python descendant from NYU's JET Information Extraction system",
+  url='https://github.com/Alex-X-W/jettie',
+  author='Xuan Wang and Jiayun Yu',
+  author_email='xwang@nyu.edu',
+  license='MIT',
+  packages=['jettie'],
+  zip_safe=False)
